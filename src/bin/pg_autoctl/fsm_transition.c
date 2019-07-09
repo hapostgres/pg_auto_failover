e10396250d354cc4efdccbdb3fc27cee0ef71f7a
Murat Tuncer <mtuncer@citusdata.com>
2019-07-09 17:18:07 +0300

    Drop replication slot after failover being demoted to secondary
    Replication slot record remains in pg_replication_slots after
    primary gets demoted to be secondary. This commit explicitly
    removes the record.

--- src/bin/pg_autoctl/fsm_transition.c
+++ src/bin/pg_autoctl/fsm_transition.c
@@ -471,6 +471,12 @@ fsm_rewind_or_init(Keeper *keeper)
 		}
 	}
 
+	if (!primary_drop_replication_slot(postgres, config->replication_slot_name))
+	{
+		log_warn("Failed to drop replication slot \"%s\" used by the standby failed",
+				  config->replication_slot_name);
+	}
+
 	return true;
 }
 
