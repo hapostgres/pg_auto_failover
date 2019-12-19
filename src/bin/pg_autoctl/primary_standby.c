c1d7ebf8893dae899c564f313ade97306ed1c39c
Dimitri Fontaine <dimitri@citusdata.com>
2019-12-19 11:38:24 +0100

    Add default log level to Postgres setup.
    For the monitor, because of the way it's typically setup, we need to
    implement our prefered logging setup. For the keeper Postgres nodes, it is
    still possible to override our choices the usual ways, so it looks okay to
    provide with a default that allows better debug facilities for us.

--- src/bin/pg_autoctl/primary_standby.c
+++ src/bin/pg_autoctl/primary_standby.c
@@ -41,6 +41,13 @@ static void local_postgres_update_pg_failures_tracking(
 	{ "hot_standby", "on" },			\
 	{ "synchronous_commit", "on" },		\
 	{ "logging_collector", "on" },		\
+	{ "log_destination", "stderr"},		\
+	{ "logging_collector", "on"},		\
+	{ "log_directory", "log"},			\
+	{ "log_min_messages", "info"},		\
+	{ "log_connections", "on"},			\
+	{ "log_disconnections", "on"},		\
+	{ "log_lock_waits", "on"},			\
 	{ "listen_addresses", "'*'" },		\
 	{ "port", "5432" }
 
