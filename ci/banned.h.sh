a91ec0ad0e4cf14038ebe5c3cdc1d426bf4732f6
Murat Tuncer <murat.tuncer@microsoft.com>
2020-03-03 14:28:51 +0300

    Exclude lib directories

--- ci/banned.h.sh
+++ ci/banned.h.sh
@@ -12,7 +12,7 @@
 
 set -eu
 
-files=$(find src -iname '*.[ch]')
+files=$(find src -iname '*.[ch]' -not -path "src/bin/lib/*")
 
 # grep is allowed to fail, that means no banned matches are found
 set +e
