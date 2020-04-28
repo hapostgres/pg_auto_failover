1bf10091aca23ac0d808da3130e3f55c9a1033b1
Dimitri Fontaine <dimitri@citusdata.com>
2020-04-28 18:11:02 +0200

    Implement SSL upgrade to verify-ca tests.

---
+++ tests/ssl_cert_utils.py
@@ -0,0 +1,100 @@
+#
+# SSL Certificate creation in the test environment
+#
+import subprocess
+import os, os.path, time, shutil
+
+
+class SSLCert():
+    """
+    Calls openssl to generate SSL certificates and sign them.
+    """
+    def __init__(self, directory, basename, CN):
+        self.directory = directory
+        self.basename = basename
+        self.CN = CN
+
+        self.csr = None
+        self.key = None
+        self.crt = None
+
+        self.rootKey = None
+        self.rootCert = None
+
+        self.sudo_mkdir_p()
+
+    def sudo_mkdir_p(self):
+        if os.path.isdir(self.directory):
+            return True
+
+        p = subprocess.Popen(["sudo", "-E", '-u', os.getenv("USER"),
+                              'env', 'PATH=' + os.getenv("PATH"),
+                              "mkdir", "-p", self.directory])
+        assert(p.wait() == 0)
+
+    def create_root_cert(self):
+        # avoid bugs where we overwrite certificates in a given directory
+        assert self.csr is None
+        assert self.crt is None
+        assert self.key is None
+
+        self.csr = os.path.join(self.directory, "%s.csr" % self.basename)
+        self.key = os.path.join(self.directory, "%s.key" % self.basename)
+        self.crt = os.path.join(self.directory, "%s.crt" % self.basename)
+
+        # first create a certificate signing request (CSR) and a public/private
+        # key file
+        print()
+        p = subprocess.Popen(["sudo", "-E", '-u', os.getenv("USER"),
+                              'env', 'PATH=' + os.getenv("PATH"),
+                              "openssl", "req", "-new", "-nodes", "-text",
+                              "-out", self.csr, "-keyout", self.key,
+                              "-subj", self.CN])
+        assert(p.wait() == 0)
+
+        p = subprocess.Popen(["chmod", "og-rwx", self.key])
+        assert(p.wait() == 0)
+
+        # Then, sign the request with the key to create a root certificate
+        # authority
+        p = subprocess.Popen(["sudo", "-E", '-u', os.getenv("USER"),
+                              'env', 'PATH=' + os.getenv("PATH"),
+                              "openssl", "x509", "-req", "-in", self.csr,
+                              "-text", "-days", "3650",
+                              "-extfile", "/etc/ssl/openssl.cnf",
+                              "-extensions", "v3_ca",
+                              "-signkey", self.key,
+                              "-out", self.crt])
+        assert(p.wait() == 0)
+
+
+    def create_signed_certificate(self, rootKey=None, rootCert=None):
+        # avoid bugs where we overwrite certificates in a given directory
+        assert self.csr is None
+        assert self.crt is None
+        assert self.key is None
+
+        self.rootKey = rootKey
+        self.rootCert = rootCert
+
+        self.crt = os.path.join(self.directory, "%s.crt" % self.basename)
+        self.csr = os.path.join(self.directory, "%s.csr" % self.basename)
+        self.key = os.path.join(self.directory, "%s.key" % self.basename)
+
+        p = subprocess.Popen(["sudo", "-E", '-u', os.getenv("USER"),
+                              'env', 'PATH=' + os.getenv("PATH"),
+                              "openssl", "req", "-new", "-nodes", "-text",
+                              "-out", self.csr, "-keyout", self.key,
+                              "-subj", self.CN])
+        assert(p.wait() == 0)
+
+        p = subprocess.Popen(["chmod", "og-rwx", self.key])
+        assert(p.wait() == 0)
+
+        p = subprocess.Popen(["sudo", "-E", '-u', os.getenv("USER"),
+                              'env', 'PATH=' + os.getenv("PATH"),
+                              "openssl", "x509", "-req", "-in", self.csr,
+                              "-text", "-days", "365",
+                              "-CA", self.rootCert, "-CAkey", self.rootKey,
+                              "-CAcreateserial", "-out", self.crt])
+        assert(p.wait() == 0)
--- tests/test_enable_ssl.py
+++ tests/test_enable_ssl.py
@@ -105,3 +105,84 @@ def test_008_disable_maintenance():
     assert node2.wait_until_pg_is_running()
     assert node2.wait_until_state(target_state="secondary")
     assert node1.wait_until_state(target_state="primary")
+
+# upgrade to verify full
+def test_009_enable_maintenance():
+    print()
+    print("Enabling maintenance on node2")
+    node2.enable_maintenance()
+
+    assert node2.wait_until_state(target_state="maintenance")
+
+def test_009_enable_ssl_verify_ca_monitor():
+    client_top_directory = os.path.join(os.getenv("HOME"), ".postgresql")
+
+    cluster.create_root_cert(client_top_directory,
+                             basename = "root",
+                             CN = "/CN=root.pgautofailover.ca")
+
+    # now create and sign the CLIENT certificate
+    clientCert = cert.SSLCert(client_top_directory,
+                              basename = "postgresql",
+                              CN = "/CN=autoctl_node")
+    clientCert.create_signed_certificate(rootKey = cluster.cert.key,
+                                         rootCert = cluster.cert.crt)
+
+    # now create and sign the SERVER certificate for the monitor
+    monitorCert = cert.SSLCert("/tmp/certs/monitor", "server",
+                              "/CN=monitor.pgautofailover.ca")
+    monitorCert.create_signed_certificate(rootKey = cluster.cert.key,
+                                          rootCert = cluster.cert.crt)
+
+    monitor.enable_ssl(sslCAFile=cluster.cert.crt,
+                       sslServerKey=monitorCert.key,
+                       sslServerCert=monitorCert.crt,
+                       sslMode="verify-ca")
+
+    monitor.sleep(2) # we signaled, wait some time
+
+    eq_(monitor.config_get("ssl.sslmode"), "verify-ca")
+    eq_(monitor.pg_config_get('ssl'), "on")
+    check_ssl_files(monitor)
+
+def test_010_enable_ssl_verify_ca_primary():
+    node1Cert = cert.SSLCert("/tmp/certs/node1", "server",
+                              "/CN=node1.pgautofailover.ca")
+    node1Cert.create_signed_certificate(rootKey = cluster.cert.key,
+                                        rootCert = cluster.cert.crt)
+
+    node1.stop_pg_autoctl()
+    node1.enable_ssl(sslSelfSigned=True, sslMode="verify-ca")
+    node1.run()
+    node1.sleep(2)
+
+    eq_(node1.config_get("ssl.sslmode"), "require")
+    assert "sslmode=verify-ca" in node1.config_get("pg_autoctl.monitor")
+    eq_(node1.pg_config_get('ssl'), "on")
+    check_ssl_files(node1)
+
+def test_011_enable_ssl_verify_ca_primary():
+    node2Cert = cert.SSLCert("/tmp/certs/node2", "server",
+                              "/CN=node2.pgautofailover.ca")
+    node2Cert.create_signed_certificate(rootKey = cluster.cert.key,
+                                        rootCert = cluster.cert.crt)
+
+    node2.enable_ssl(sslSelfSigned=True, sslMode="require")
+    node2.sleep(5)
+
+    eq_(node2.config_get("ssl.sslmode"), "verify-ca")
+
+    node2.wait_until_pg_is_running()
+
+    eq_(node2.pg_config_get('ssl'), "on")
+    check_ssl_files(node2)
+
+    if node2.pgmajor() >= 12:
+        assert "sslmode=verify-ca" in node2.pg_config_get('primary_conninfo')
+
+def test_012_disable_maintenance():
+    print("Disabling maintenance on node2")
+    node2.disable_maintenance()
+    assert node2.wait_until_pg_is_running()
+    assert node2.wait_until_state(target_state="secondary")
+    assert node1.wait_until_state(target_state="primary")
