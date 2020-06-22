85f6a7005b4a48a2c7d61c5ae6c2d2e78eb79cd4
Dimitri Fontaine <dimitri@citusdata.com>
2020-06-22 15:52:17 +0200

    Write some tests for pg_autoctl enable maintenance --allow-failover.

--- tests/test_basic_operation.py
+++ tests/test_basic_operation.py
@@ -53,7 +53,7 @@ def test_005_read_from_secondary():
 def test_006_writes_to_node2_fail():
     node2.run_sql_query("INSERT INTO t1 VALUES (3)")
 
-def test_007_maintenance():
+def test_007_maintenance_secondary():
     print()
     print("Enabling maintenance on node2")
     assert node1.wait_until_state(target_state="primary")
@@ -68,35 +68,55 @@ def test_007_maintenance():
     assert node2.wait_until_state(target_state="secondary")
     assert node1.wait_until_state(target_state="primary")
 
-def test_008_fail_primary():
+def test_008_maintenance_primary():
+    print()
+    print("Enabling maintenance on node1, allowing failover")
+    assert node1.wait_until_state(target_state="primary")
+    node1.enable_maintenance(allowFailover=True)
+    assert node1.wait_until_state(target_state="maintenance")
+    assert node2.wait_until_state(target_state="wait_primary")
+
+    print("Disabling maintenance on node1")
+    node1.disable_maintenance()
+    assert node1.wait_until_pg_is_running()
+    assert node1.wait_until_state(target_state="secondary")
+    assert node2.wait_until_state(target_state="primary")
+
+def test_009_failback():
+    print()
+    monitor.failover()
+    assert node2.wait_until_state(target_state="secondary")
+    assert node1.wait_until_state(target_state="primary")
+
+def test_010_fail_primary():
     print()
     print("Injecting failure of node1")
     node1.fail()
     assert node2.wait_until_state(target_state="wait_primary")
 
-def test_009_writes_to_node2_succeed():
+def test_011_writes_to_node2_succeed():
     node2.run_sql_query("INSERT INTO t1 VALUES (4)")
     results = node2.run_sql_query("SELECT * FROM t1 ORDER BY a")
     assert results == [(1,), (2,), (3,), (4,)]
 
-def test_010_start_node1_again():
+def test_012_start_node1_again():
     node1.run()
     assert node2.wait_until_state(target_state="primary")
     assert node1.wait_until_state(target_state="secondary")
 
-def test_011_read_from_new_secondary():
+def test_013_read_from_new_secondary():
     results = node1.run_sql_query("SELECT * FROM t1 ORDER BY a")
     assert results == [(1,), (2,), (3,), (4,)]
 
 @raises(Exception)
-def test_012_writes_to_node1_fail():
+def test_014_writes_to_node1_fail():
     node1.run_sql_query("INSERT INTO t1 VALUES (3)")
 
-def test_013_fail_secondary():
+def test_015_fail_secondary():
     node1.fail()
     assert node2.wait_until_state(target_state="wait_primary")
 
-def test_014_drop_secondary():
+def test_016_drop_secondary():
     node1.run()
     assert node1.wait_until_state(target_state="secondary")
     node1.drop()
@@ -106,16 +126,16 @@ def test_014_drop_secondary():
     # replication slot list should be empty now
     assert node2.has_needed_replication_slots()
 
-def test_015_add_new_secondary():
+def test_017_add_new_secondary():
     global node3
     node3 = cluster.create_datanode("/tmp/basic/node3")
     node3.create()
 
 @raises(Exception)
-def test_016_cant_failover_yet():
+def test_018_cant_failover_yet():
     monitor.failover()
 
-def test_017_run_secondary():
+def test_019_run_secondary():
     node3.run()
     assert node3.wait_until_state(target_state="secondary")
     assert node2.wait_until_state(target_state="primary")
@@ -132,7 +152,7 @@ def test_017_run_secondary():
 # replication, we check that we still have a replication slot for the other
 # node.
 #
-def test_018_multiple_manual_failover_verify_replication_slots():
+def test_020_multiple_manual_failover_verify_replication_slots():
     print()
 
     print("Calling pgautofailover.failover() on the monitor")
@@ -151,12 +171,12 @@ def test_018_multiple_manual_failover_verify_replication_slots():
     assert node2.has_needed_replication_slots()
     assert node3.has_needed_replication_slots()
 
-def test_019_drop_primary():
+def test_021_drop_primary():
     node2.drop()
     assert not node2.pg_is_running()
     assert node3.wait_until_state(target_state="single")
 
-def test_020_stop_postgres_monitor():
+def test_022_stop_postgres_monitor():
     original_state = node3.get_state()
     monitor.stop_postgres()
     monitor.wait_until_pg_is_running()
