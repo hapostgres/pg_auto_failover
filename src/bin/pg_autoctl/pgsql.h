671ed9e443355f38debc4a4742ec0c1adc9f6de0
Dimitri Fontaine <dimitri@citusdata.com>
2020-06-29 11:39:00 +0200

    Reduce how often we exec() pg_controldata.
    When Postgres is known to be running we can update our control data
    information in the cache from a SQL query rather than doing another fork()
    and exec() on the pg_controldata binary and parsing its output.
    
    There are still a few too many calls to pg_controldata after this patch, but
    at least we don't do it every second anymore when Postgres is running fine.

--- src/bin/pg_autoctl/pgsql.h
+++ src/bin/pg_autoctl/pgsql.h
@@ -259,7 +259,8 @@ bool pgsql_reset_primary_conninfo(PGSQL *pgsql);
 
 bool pgsql_get_postgres_metadata(PGSQL *pgsql, const char *slotName,
 								 bool *pg_is_in_recovery,
-								 char *pgsrSyncState, char *currentLSN);
+								 char *pgsrSyncState, char *currentLSN,
+								 PostgresControlData *control);
 
 bool pgsql_one_slot_has_reached_target_lsn(PGSQL *pgsql,
 										   char *targetLSN,
