bebc898d42dbd621bda39d19fcd48320f685d950
Dimitri Fontaine <dimitri@citusdata.com>
2020-09-17 18:17:00 +0200

    Clean-up some debug messages.

--- src/monitor/health_check_worker.c
+++ src/monitor/health_check_worker.c
@@ -493,9 +493,6 @@ HealthCheckWorkerMain(Datum arg)
 		{
 			List *nodeHealthList = LoadNodeHealthList();
 
-			elog(NOTICE, "HealthCheckWorkerMain loaded %d nodes",
-				 list_length(nodeHealthList));
-
 			if (nodeHealthList != NIL)
 			{
 				List *healthCheckList = CreateHealthChecks(nodeHealthList);
