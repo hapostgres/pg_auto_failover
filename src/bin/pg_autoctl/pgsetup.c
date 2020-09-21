245dbfc074c08e004239801eb9ca3042791ed5e9
Dimitri Fontaine <dimitri@citusdata.com>
2020-09-21 18:14:57 +0200

    Per review.

--- src/bin/pg_autoctl/pgsetup.c
+++ src/bin/pg_autoctl/pgsetup.c
@@ -830,8 +830,9 @@ pg_setup_get_local_connection_string(PostgresSetup *pgSetup,
 
 
 /*
- * pg_setup_pgdata_exists returns true when the pg_controldata probe was
- * susccessful.
+ * pg_setup_pgdata_exists returns true when PGDATA exists, hosts a
+ * global/pg_control file (so that it looks like a Postgres cluster) and when
+ * the pg_controldata probe was successful.
  */
 bool
 pg_setup_pgdata_exists(PostgresSetup *pgSetup)
@@ -860,7 +861,10 @@ pg_setup_pgdata_exists(PostgresSetup *pgSetup)
 	{
 		bool missingPgdataIsOk = false;
 
-		return pg_controldata(pgSetup, missingPgdataIsOk);
+		/* errors are logged from within pg_controldata */
+		(void) pg_controldata(pgSetup, missingPgdataIsOk);
+
+		return pgSetup->control.system_identifier != 0;
 	}
 
 	return true;
