f36fb311b377518614a2b1479472c79ddf353bc3
Joe Nelson <48225118+jonels-msft@users.noreply.github.com>
2021-04-29 14:00:17 -0500

    Improve docs color contrast for accessibility (#674)
    * Fix contrast with Citus docs color theme
    
    * Make keyboard focus visible on next,prev buttons
    
    * Force the latest ver of sphinx and theme
    
    * Remove Citus-specific extra styles
    
    * app.add_stylesheet has been deprecated
    
    * Tell RTD where to find our package/config preferences
    
    * Reformat config.py with "black"

--- docs/conf.py
+++ docs/conf.py
@@ -107,6 +107,12 @@ html_theme = "sphinx_rtd_theme"
 #
 # html_theme_options = {}
 
+# Add our custom CSS
+def setup(app):
+    app.add_css_file("css/citus.css")
+    app.add_css_file("css/pygments.css")
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
