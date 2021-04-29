b3a2bf1056254a1c9815008552c7775d2da98346
Joe Nelson <jonels@microsoft.com>
2021-04-29 13:40:46 -0500

    Reformat config.py with "black"

--- docs/conf.py
+++ docs/conf.py
@@ -109,8 +109,9 @@ html_theme = "sphinx_rtd_theme"
 
 # Add our custom CSS
 def setup(app):
-  app.add_css_file('css/citus.css')
-  app.add_css_file('css/pygments.css')
+    app.add_css_file("css/citus.css")
+    app.add_css_file("css/pygments.css")
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
