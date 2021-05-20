125bb19c13ea8af1d6aacd5f2f9f7cd51b406e74
Dimitri Fontaine <dimitri@citusdata.com>
2021-05-20 17:16:25 +0200

    Prepare/release 1.5.2 (#710)
    * Update changelog.
    
    * Update parson JSON parsing lib.
    
    * Update version numbers.

--- src/bin/pg_autoctl/defaults.h
+++ src/bin/pg_autoctl/defaults.h
@@ -14,7 +14,7 @@
 #define PG_AUTOCTL_STATE_VERSION 1
 
 /* additional version information for printing version on CLI */
-#define PG_AUTOCTL_VERSION "1.5.1"
+#define PG_AUTOCTL_VERSION "1.5.2"
 
 /* version of the extension that we requite to talk to on the monitor */
 #define PG_AUTOCTL_EXTENSION_VERSION "1.5"
