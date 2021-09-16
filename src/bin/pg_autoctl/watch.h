fa5eac6b99484b46ec220f94a6b988be6a4b877c
Dimitri Fontaine <dimitri@citusdata.com>
2021-09-16 15:26:46 +0200

    Organize the display code better, print state from the monitor.

---
+++ src/bin/pg_autoctl/watch.h
@@ -0,0 +1,51 @@
+/*
+ * src/bin/pg_autoctl/watch.h
+ *     Implementation of a CLI to show events, states, and URI from the
+ *     pg_auto_failover monitor.
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the PostgreSQL License.
+ *
+ */
+
+#ifndef WATCH_H
+#define WATCH_H
+
+#include <ncurses.h>
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "defaults.h"
+#include "monitor.h"
+#include "nodestate_utils.h"
+
+/* a ring-buffer of the last 1024 monitor events */
+typedef struct EventsBuffer
+{
+	int pos;
+	CurrentNodeState events[BUFSIZE];
+} EventsBuffer;
+
+typedef struct WatchContext
+{
+	int rows;
+	int cols;
+
+	Monitor *monitor;
+	char formation[NAMEDATALEN];
+	int groupId;
+	CurrentNodeStateArray nodesArray;
+	EventsBuffer eventsBuffer;
+} WatchContext;
+
+
+void watch_main_loop(Monitor *monitor, char *formation, int groupId);
+void watch_init_window(WatchContext *context);
+void watch_end_window(WatchContext *context);
+
+bool watch_update(WatchContext *context);
+bool watch_render(WatchContext *context);
+
+#endif  /* WATCH_H */
