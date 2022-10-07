1424803b80587c67c3004c7304edaf1785370861
Dimitri Fontaine <dimitri@citusdata.com>
2022-10-07 13:10:14 +0200

    Release v2.0.

---
+++ src/monitor/pgautofailover--1.6--2.0.sql
@@ -0,0 +1,7 @@
+--
+-- extension update file from 1.6 to 2.0
+--
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pgautofailover" to load this file. \quit
+
+-- no changes, just the version number
