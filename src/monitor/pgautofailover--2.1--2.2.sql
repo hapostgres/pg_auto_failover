8322677fcd73864b5183d2f2a3685adf4ba10e49
Justin Kromlinger <mail@hashworks.net>
2025-04-03 14:05:01 +0200

    Prepare for release 2.2 (#1069)
    * Prepare for release 2.2
    
    * Add CHANGELOG entry for the release
    * Update version number throughout
    * Update version related tests
    
    * Add 2.0 -> 2.1 sql file to fix update path
    
    * Add 2.1 -> 2.2 sql file, move dummy
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Mateusz Prais <uamfhq@gmail.com>
    Co-authored-by: Dimitri Fontaine <dim@tapoueh.org>

---
+++ src/monitor/pgautofailover--2.1--2.2.sql
@@ -0,0 +1,7 @@
+--
+-- extension update file from 2.1 to 2.2
+--
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pgautofailover" to load this file. \quit
+
+-- no changes, just the version number
