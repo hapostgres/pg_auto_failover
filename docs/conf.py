b7a67f8e93a6e5d7e69a393ffdf33f54e5f661f0
Dimitri Fontaine <dim@tapoueh.org>
2026-07-06 22:41:26 +0200

    docs: fix two conf.py warnings (invalid escape, language=None)

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ release = "2.2"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -148,7 +148,7 @@ latex_documents = [
     (
         master_doc,
         "pg_auto_failover.tex",
-        "pg\_auto\_failover Documentation",
+        r"pg\_auto\_failover Documentation",
         "Microsoft",
         "manual",
     ),
